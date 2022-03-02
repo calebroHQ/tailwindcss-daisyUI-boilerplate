5ae9e46d686aae2c7453fc23466aa778bec95cf7
calebro <calebro@localhost.localdomain>
2022-03-02 10:05:29 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+ purge: {
+  node: 'layers',
+  content: ['./public/**/*.html'],
+ },
+ darkMode: false, // or 'media' or 'class'
+ theme: {
+  extend: {
+  color: {}
+ },
+},
+variants: {
+ extend: {},
+},
+plugins: [],
+}
+module.exports = {
+
+    plugins: [
+      require('daisyui'),
+    ],
+
+  }
